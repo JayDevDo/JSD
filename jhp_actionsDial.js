97ea6cf58bf44804bd0723c95e1a1767c155772d
Jay <42993384+JayDevDo@users.noreply.github.com>
2024-05-02 17:30:38 +0200

    removed drag

--- jhp_actionsDial.js
+++ jhp_actionsDial.js
@@ -5,25 +5,6 @@
 	
 */
 
-
-/* 	re-order pages/dials */
-dragButton = ()=>{
-
-	$( "button" ).draggable();
-	
-	$( ".pageTable" ).droppable(
-		{	drop: 	function( event, ui ) {
-						$( this )
-							.addClass( "ui-state-highlight" )
-							.find( "p" )
-							.html( "Dropped!" );
-					}
-		}
-	)
-
-}
-
-
 openDial = ( tileIdx )=>{
 	window.open( appTiles[tileIdx].url , target=appSettings.newtab );
 	set_LS_LastVisit(tileIdx) ;
